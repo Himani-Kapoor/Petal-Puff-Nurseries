8aa62c77ea48939aa81d69df6b32e30d31fe03d8
Himani Kapoor <himanikapoor482@gmail.com>
2026-06-08 23:08:00 +0530

    ------

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import UserRegister from '../pages/UserRegister'
 import UserLogin from '../pages/UserLogin'
 import NurseryOwnerRegister from '../pages/NurseryOwnerRegister'
@@ -8,23 +8,28 @@ import NurseryOwnerLogin from '../pages/NurseryOwnerLogin'
 
 const AppRoutes = () => {
   return (
+    <>
     <Router>
         <Routes>
             <Route path="/user/register" element={<UserRegister/>}/>
             <Route path="/user/login" element={<UserLogin/>}/>
             <Route path="/nursery-owner/register" element={<NurseryOwnerRegister/>}/>
-            <Route path="nursery-owner/login" element={<NurseryOwnerLogin/>}/>
+            <Route path="/nursery-owner/login" element={<NurseryOwnerLogin/>}/>
 
-          <button>User Registration</button>
-          <button>User Login</button>
-          <button>Nursery Owner Registration</button>
-          <button>Nursery Owner Login</button>
+          
 
 
 
         </Routes>
       
     </Router>
+    <marquee behavior="" direction=""><h1>hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello</h1></marquee>
+
+
+    <div>
+      
+    </div>
+    </>
   )
 }
 
